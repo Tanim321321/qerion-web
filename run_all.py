855fed3081c11640644c97a5415929f8b7a72af8
Tanim321321 <98247990+Tanim321321@users.noreply.github.com>
2025-06-15 10:21:25 +0600

    update run_all.py

--- run_all.py
+++ run_all.py
@@ -5,7 +5,7 @@ def run_bot():
     subprocess.run(["python", "main.py"])
 
 def run_ui():
-    subprocess.run(["python", "web/web_ui.py"])
+    subprocess.run(["python", "-m", "flask", "--app", "web_ui", "run", "--host=0.0.0.0", "--port=10000"])
 
 bot_thread = threading.Thread(target=run_bot)
 ui_thread = threading.Thread(target=run_ui)
